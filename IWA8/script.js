3b834a173d4413b24468e550663beb1b357fe2d9
Cole Miles <cole@codespace.co.za>
2023-05-23 12:39:04 +0200

    Added IWA13

--- IWA8/script.js
+++ IWA8/script.js
@@ -26,7 +26,7 @@ const leo = {
 };
 
 const sarah = {
-  name: sarahName.trim() + sarahSurname,
+  name: sarahName.trim() + " " + sarahSurname,
   age: 62,
   accessID: "6b279ae5-5657-4240-80e9-23f6b635f7a8",
   balance: sarahBalance,
