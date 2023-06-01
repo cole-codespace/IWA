489558b0a4b906059c548a3c0c33a11eaac7ff8d
Cole Miles <cole@codespace.co.za>
2023-06-01 17:00:11 +0200

    Fixed IWA12

--- IWA12/script.js
+++ IWA12/script.js
@@ -1,43 +1,43 @@
 const STATUS_MAP = {
   shelf: {
-      color: 'green',
-      canReserve: true,
-      canCheckout: true,
-      canCheckIn: false,
+    color: 'green',
+    canReserve: true,
+    canCheckout: true,
+    canCheckIn: false,
   },
   reserved: {
-      color: 'blue',
-      canReserve: false,
-      canCheckout: true,
-      canCheckIn: false,
+    color: 'blue',
+    canReserve: false,
+    canCheckout: true,
+    canCheckIn: false,
   },
   overdue: {
-      color: 'red',
-      canReserve: false,
-      canCheckout: false,
-      canCheckIn: true,
+    color: 'red',
+    canReserve: false,
+    canCheckout: false,
+    canCheckIn: true,
   },
   checkedOut: {
-      color: 'orange',
-      canReserve: true,
-      canCheckout: false,
-      canCheckIn: false,
+    color: 'orange',
+    canReserve: true,
+    canCheckout: false,
+    canCheckIn: false,
   }
 }
 
-// Edit below line 
+//Edit below line 
 
-const statuses = document.querySelectorAll('.status');                //put all together as one memory in a block. //querySelect all .status, .reserve, .checkout, .checkin.
+const statuses = document.querySelectorAll('.status');              
 const reserveButtons = document.querySelectorAll('.reserve');
 const checkoutButtons = document.querySelectorAll('.checkout');
 const checkinButtons = document.querySelectorAll('.checkin');
 
-statuses.forEach(function(status, index) {                            //add function to add status and index file of the html  .forEach Index of Books 1, 2, 3.   
-const bookStatus = status.textContent;
-const statusInfo = STATUS_MAP[bookStatus];                          //status map is a visual representation that shows the progress or status of different tasks, milestones, or features within a project.
+statuses.forEach(function (status, index) {                          
+  const bookStatus = status.textContent;
+  const statusInfo = STATUS_MAP[bookStatus];                          
 
-status.style.color = statusInfo.color;
-reserveButtons[index].disabled = !statusInfo.canReserve;           //check the index for status of element.
-checkoutButtons[index].disabled = !statusInfo.canCheckout;
-checkinButtons[index].disabled = !statusInfo.canCheckIn;
+  status.style.color = statusInfo.color;
+  reserveButtons[index].disabled = !statusInfo.canReserve;          
+  checkoutButtons[index].disabled = !statusInfo.canCheckout;
+  checkinButtons[index].disabled = !statusInfo.canCheckIn;
 });
